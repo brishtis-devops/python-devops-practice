2b6453a1e44356274cd6e52c7f1418ccb2942731
Brishti Saha <brishtis154@gmail.com>
2026-03-13 15:05:25 +0000

    First Script

---
+++ 01_systeminfo.py
@@ -0,0 +1,11 @@
+import socket
+
+hostname = socket.gethostname()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip_address = s.getsockname()[0]
+s.close()
+
+print(f"Hostname: {hostname}")
+print(f"IP Address: {ip_address}")
+
